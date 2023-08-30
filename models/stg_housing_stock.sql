322e721dcce102f0b6038b9c8da6e787ccae0101
Nicolas <glaus.nicolas@gmail.com>
2023-08-30 14:17:02 +0000

    housing_stock cleaned

---
+++ models/stg_housing_stock.sql
@@ -0,0 +1,11 @@
+select
+    municipality_code,
+    year,
+    nb_principal_home,
+    nb_second_home,
+    nb_vacants_housing,
+    nb_tot_housing,
+    secondary_home_rate,
+    principal_home_rate,
+    vacants_housing_rate,
+from prello-cna.Prello_cna.housing_stock
