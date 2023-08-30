88c76bb0c276f48e76fc65166ed7f5673cb9874a
chloesofiawegener@gmail.com <chloesofiawegener@gmail.com>
2023-08-30 12:16:10 +0000

    Clean updated.

--- models/geo_data.sql
+++ models/geo_data.sql
@@ -6,7 +6,7 @@ municipality_type,
 latitude,			
 longitude,									
 department_name,	
-CAST(department_code AS INT64) AS department_code
+department_code
 FROM Prello_cna.geographical_referential
 WHERE department_code NOT IN ('971', '972', '973', '974', '976', '977', '978', '986', '987', '988')
 
