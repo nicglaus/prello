9731a822f9705569f0b5b63f2410b65d220abe2c
chloesofiawegener@gmail.com <chloesofiawegener@gmail.com>
2023-08-31 11:58:56 +0000

    fixed geo_data with country_name column for Looker Studio

--- models/geo_data.sql
+++ models/geo_data.sql
@@ -7,6 +7,6 @@ longitude,
 department_name,	
 department_code,
 country_name
-FROM Prello_cna.geographical_referential
+FROM Prello_cna.geo_referential
 WHERE department_code NOT IN ('971', '972', '973', '974', '976', '977', '978', '986', '987', '988')
 
